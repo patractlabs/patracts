a14c023cd676823495ceceae7035af2df93fc9fd
zhangtailin <tailinzhang1993@gmail.com>
2021-04-15 16:19:10 +0800

    fix deposit refund

--- runtime/patracts/src/lib.rs
+++ runtime/patracts/src/lib.rs
@@ -281,6 +281,7 @@ pub type SignedExtra = (
 	frame_system::CheckNonce<Runtime>,
 	frame_system::CheckWeight<Runtime>,
 	pallet_transaction_payment::ChargeTransactionPayment<Runtime>,
+	pallet_contracts::ChargeDepositPayment<Runtime>,
 );
 /// Unchecked extrinsic type as expected by this runtime.
 pub type UncheckedExtrinsic = generic::UncheckedExtrinsic<Address, Call, Signature, SignedExtra>;
